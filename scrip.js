5071dc73f64d1aa4df212c3bcadfffe23221b5aa
NikitaEvchuk <144231215+NikitaEvchuk@users.noreply.github.com>
2023-09-14 16:48:38 +0300

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,37 @@
+let age = 16
+console.log(age) 
+
+
+let name = "Nikita"
+console.log(name)
+
+let isStudend = false 
+console.log(isStudend)
+
+let myString = "Are you strong ? "
+console.log(myString)
+
+
+let myNumber = 7
+console.log(myNumber + 10)
+
+let myNull = null 
+console.log(myNull) 
+
+
+
+let question = prompt("What is your name?")
+console.log("Hello " + question) 
+
+let confirmation = confirm("Підтвердіть свою особу")
+if(confirmation){
+    console.log("Дякую за підтвердження!")
+} else console.log("Дію відмінено!")
+
+let attention = alert("Be careful")
+let confirmation = confirm("Підтвердіть особу")
+if(confirmation){
+    console.log( "Дякую за підтвердження!")
+} else console.log("Дію відмінено!")   
+
+
